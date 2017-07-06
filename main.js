f3238445c69ffc3856dac6117a10a418128c948b
matthewtessler <matthewtessler96@gmail.com>
2017-07-06 17:23:45 -0400

    changed to bootstrap button for search

--- main.js
+++ main.js
@@ -2,9 +2,16 @@ var Spotify = require('spotify-web-api-js');
 var s = new Spotify();
 s.setAccessToken(token);
 
+document.getElementById("goBtn").addEventListener("click", searching);
+document.getElementById("searchBar").addEventListener("keyup", function(e) {
+  if (e.keyCode == 13) {
+    searching();
+  }
+});
+
 function searching(){
   // disable search button for now so function won't run again unless new search
-  var searchButton = document.getElementById('searchButton');
+  var searchButton = document.getElementById('goBtn');
   searchButton.disabled = true;
 
   // get the search query and send to spotify api search endpoint
@@ -23,13 +30,7 @@ function searching(){
       tracks.innerHTML = '<h2>Tracks</h2>';
       var tracksList = data.tracks.items;
       tracksList.forEach(function(ele){
-        /*
-        var mediaDiv = "<div class='media'><div class='media-left'><img class='media-object' src='";
-        mediaDiv += "https://www.neto.com.au/assets/images/default_product.gif";
-        mediaDiv +="' alt='" + ele.name + "'></div><div class='media-body'><p>" + ele.name + "</p></div></div>";
-        tracks.innerHTML += mediaDiv;*/
         tracks.innerHTML += "<p>" + ele.name + "</p>";
-
       });
 
       // artists div will hold artists results
@@ -38,16 +39,6 @@ function searching(){
       artists.innerHTML = '<h2>Artists</h2>';
       var artistsList = data.artists.items;
       artistsList.forEach(function(ele){
-        /*
-        var mediaDiv = "<div class='media'><div class='media-left'><img class='media-object' src='";
-        if (typeof(ele.images[2].url) !== 'undefined') {
-          mediaDiv += ele.images[2].url;
-        }
-        else {
-          mediaDiv += "https://www.neto.com.au/assets/images/default_product.gif";
-        }
-        mediaDiv +="' alt='" + ele.name + "'></div><div class='media-body'><p>" + ele.name + "</p></div></div>";
-        artists.innerHTML += mediaDiv; */
         artists.innerHTML += "<p>" + ele.name + "</p>";
       });
 
@@ -57,18 +48,7 @@ function searching(){
       albums.innerHTML = '<h2>Albums</h2>';
       var albumsList = data.albums.items;
       albumsList.forEach(function(ele){
-        /*
-        var mediaDiv = "<div class='media'><div class='media-left'><img class='media-object' src='";
-        if (typeof(ele.images[2].url) !== 'undefined') {
-          mediaDiv += ele.images[2].url;
-        }
-        else {
-          mediaDiv += "https://www.neto.com.au/assets/images/default_product.gif";
-        }
-        mediaDiv +="' alt='" + ele.name + "'></div><div class='media-body'><p>" + ele.name + "</p></div></div>";
-        albums.innerHTML += mediaDiv; */
         albums.innerHTML += "<p>" + ele.name + "</p>";
-
       });
 
       // find the search results div and append all the results to it
@@ -79,5 +59,3 @@ function searching(){
     }
   });
 }
-
-document.getElementById("searchButton").addEventListener("click", searching);
