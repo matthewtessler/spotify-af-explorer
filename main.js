b42dcfca9ef83a0435a7089a3c1eac864f756d23
matthewtessler <matthewtessler96@gmail.com>
2017-07-07 11:13:08 -0400

    audio features now accessible for tracks

--- main.js
+++ main.js
@@ -24,19 +24,38 @@ function searching(){
       console.log(data);
 
       var tracksList = data.tracks.items;
-      tracksList.forEach(function(ele){
-        searchResultsDiv.innerHTML += "<div class='col-lg-3'><div class='panel panel-default'><div class='panel-heading'>" + ele.name + " <small>Track</small></span></div></div></div>";
-      });
-
       var artistsList = data.artists.items;
-      artistsList.forEach(function(ele){
-        searchResultsDiv.innerHTML += "<div class='col-lg-3'><div class='panel panel-default'><div class='panel-heading'>" + ele.name + " <small>Artist</small></div></div></div>";
+      var albumsList = data.albums.items;
+
+      var ids = [];
+      tracksList.forEach(function(ele) {
+        ids.push(ele.id);
+      })
+
+      s.getAudioFeaturesForTracks(ids, function(err,data) {
+        if (err) {
+          console.error(err);
+        }
+        else {
+          console.log(data);
+          for (var i=0; i < tracksList.length; i++) {
+            searchResultsDiv.innerHTML += "<div class='col-lg-3'><div class='panel panel-default'><div class='panel-heading'>" + tracksList[i].name + " <small>Track</small></div><div class='panel-body'>";
+            // audio features parsing will go here
+            searchResultsDiv.innerHTML += "</div></div></div>";
+          }
+
+          artistsList.forEach(function(ele){
+            searchResultsDiv.innerHTML += "<div class='col-lg-3'><div class='panel panel-default'><div class='panel-heading'>" + ele.name + " <small>Artist</small></div></div></div>";
+            // artists will have an average audio features of all their songs, or something
+          });
+
+          albumsList.forEach(function(ele){
+            searchResultsDiv.innerHTML += "<div class='col-lg-3'><div class='panel panel-default'><div class='panel-heading'>" + ele.name + " <small>Album</small></div></div></div>";
+            // album will have an audio features average for all tracks
+          });
+        }
       });
 
-      var albumsList = data.albums.items;
-      albumsList.forEach(function(ele){
-        searchResultsDiv.innerHTML += "<div class='col-lg-3'><div class='panel panel-default'><div class='panel-heading'>" + ele.name + " <small>Album</small></div></div></div>";
-      });
     }
   });
 }
