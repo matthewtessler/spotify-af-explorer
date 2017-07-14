1ad9c124e738f8df2f7c70e4d6fb61fe5a1178d6
matthewtessler <matthewtessler96@gmail.com>
2017-07-14 14:15:01 -0400

    added if statements for preview link and artwork and made partial fix for panel heading height

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var Spotify = require('spotify-web-api-js');
 var s = new Spotify();
-s.setAccessToken(token);
+s.setAccessToken(token); // setting access token for api calls, brought from server
 
+// event listeners for go button click and enter button click
 document.getElementById("goBtn").addEventListener("click", searching);
 document.getElementById("searchBar").addEventListener("keyup", function(e) {
   if (e.keyCode == 13) {
@@ -9,11 +10,13 @@ document.getElementById("searchBar").addEventListener("keyup", function(e) {
   }
 });
 
+// helper function builds a progress bar
 function pb(percentage, color, desc) {
   return "<div class='progress' title=" + desc + "><div class='progress-bar' role='progressbar' aria-valuenow='60' aria-valuemin='0' aria-valuemax='100' style='width:" + percentage.toFixed(0) +  "%;background-color:#" + color + "'> " +  percentage.toFixed(0) + "% " + desc + "</div></div>";
 }
 function searching(){
 
+  // getting the search results div to add the results to later
   var searchResultsDiv = document.getElementById('searchResultsDiv');
   searchResultsDiv.innerHTML = "";
 
@@ -32,19 +35,21 @@ function searching(){
       console.error(err);
     }
     else {
-      // display search results of tracks
+      // display search results of tracks on console
       console.log(data);
 
+      // grabbing the track ids to use for the audio features api call
       var tracksList = data.tracks.items;
-
       var ids = [];
       tracksList.forEach(function(ele) {
         ids.push(ele.id);
       })
 
       s.getAudioFeaturesForTracks(ids, function(err,data) {
+        // if the data shows up, display it
         if (err) {
           console.error(err);
+          searchResultsDiv.innerHTML = "<p>Could not fetch audio features. Refresh the page and try again, please.</p>";
         }
         else {
           console.log(data);
@@ -57,13 +62,25 @@ function searching(){
             var live = pb(parseFloat(data.audio_features[i].liveness)*100, "AC2F95", " &#x1F3A4"); // purple
             var speechy = pb(parseFloat(data.audio_features[i].speechiness)*100, "F39A35", " &#x1F4AC"); // orange
             var attributes = danceable + acoustic + energy + instrumental + live + speechy;
-            var preview_link = " <a href='" + tracksList[i].preview_url + "' target='_blank'><span class='glyphicon glyphicon-music' aria-hidden='true'></span></a>";
-            var artwork = "<img src='" + tracksList[i].album.images[0].url + "'>";
-            searchResultsDiv.innerHTML += "<div class='col-lg-3'><div class='panel panel-default'><div class='panel-heading'><div class='row'><div class='col-lg-9'>" + tracksList[i].name + " <a href='" + tracksList[i].external_urls.spotify +"' target='_blank'><span class='glyphicon glyphicon-new-window' aria-hidden='true'></span></a>" + preview_link + "<br>Artist: " + tracksList[i].artists[0].name + "</div><div class='col-lg-3'>" + artwork + "</div></div><div class='panel-body'>" + attributes + "</div></div></div>";
+            // check if album image exists before the tag is created for it
+            if (tracksList[i].album.images[0].url) {
+              var artwork = "<img src='" + tracksList[i].album.images[0].url + "'>";
+            }
+            else {
+              var artwork = "";
+            }
+            // check if the preview exists before building a link for it
+            if (tracksList[i].preview_url) {
+              var preview_link = " <a href='" + tracksList[i].preview_url + "' target='_blank'><span class='glyphicon glyphicon-music' aria-hidden='true'></span></a>";
+              searchResultsDiv.innerHTML += "<div class='col-lg-3'><div class='panel panel-default'><div class='panel-heading'><div class='row'><div class='col-lg-9'>" + tracksList[i].name + " <a href='" + tracksList[i].external_urls.spotify +"' target='_blank'><span class='glyphicon glyphicon-new-window' aria-hidden='true'></span></a>" + preview_link + "<br>Artist: " + tracksList[i].artists[0].name + "</div><div class='col-lg-3'>" + artwork + "</div></div></div><div class='panel-body'>" + attributes + "</div></div>";
+            }
+            else {
+              searchResultsDiv.innerHTML += "<div class='col-lg-3'><div class='panel panel-default'><div class='panel-heading'><div class='row'><div class='col-lg-9'>" + tracksList[i].name + "<a href='" + tracksList[i].external_urls.spotify + "' target='_blank'><span class='glyphicon glyphicon-new-window' aria-hidden='true'></span></a><br>Artist: " + tracksList[i].artists[0].name + "</div><div class='col-lg-3'>" + artwork + "</div></row></div></div><div class='panel-body'>" + attributes + "</div></div>";
+
+            }
           }
         }
       });
-
     }
   });
 }
--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1,8 +1,9 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var Spotify = require('spotify-web-api-js');
 var s = new Spotify();
-s.setAccessToken(token);
+s.setAccessToken(token); // setting access token for api calls, brought from server
 
+// event listeners for go button click and enter button click
 document.getElementById("goBtn").addEventListener("click", searching);
 document.getElementById("searchBar").addEventListener("keyup", function(e) {
   if (e.keyCode == 13) {
@@ -10,11 +11,13 @@ document.getElementById("searchBar").addEventListener("keyup", function(e) {
   }
 });
 
+// helper function builds a progress bar
 function pb(percentage, color, desc) {
   return "<div class='progress' title=" + desc + "><div class='progress-bar' role='progressbar' aria-valuenow='60' aria-valuemin='0' aria-valuemax='100' style='width:" + percentage.toFixed(0) +  "%;background-color:#" + color + "'> " +  percentage.toFixed(0) + "% " + desc + "</div></div>";
 }
 function searching(){
 
+  // getting the search results div to add the results to later
   var searchResultsDiv = document.getElementById('searchResultsDiv');
   searchResultsDiv.innerHTML = "";
 
@@ -33,19 +36,21 @@ function searching(){
       console.error(err);
     }
     else {
-      // display search results of tracks
+      // display search results of tracks on console
       console.log(data);
 
+      // grabbing the track ids to use for the audio features api call
       var tracksList = data.tracks.items;
-
       var ids = [];
       tracksList.forEach(function(ele) {
         ids.push(ele.id);
       })
 
       s.getAudioFeaturesForTracks(ids, function(err,data) {
+        // if the data shows up, display it
         if (err) {
           console.error(err);
+          searchResultsDiv.innerHTML = "<p>Could not fetch audio features. Refresh the page and try again, please.</p>";
         }
         else {
           console.log(data);
@@ -58,13 +63,25 @@ function searching(){
             var live = pb(parseFloat(data.audio_features[i].liveness)*100, "AC2F95", " &#x1F3A4"); // purple
             var speechy = pb(parseFloat(data.audio_features[i].speechiness)*100, "F39A35", " &#x1F4AC"); // orange
             var attributes = danceable + acoustic + energy + instrumental + live + speechy;
-            var preview_link = " <a href='" + tracksList[i].preview_url + "' target='_blank'><span class='glyphicon glyphicon-music' aria-hidden='true'></span></a>";
-            var artwork = "<img src='" + tracksList[i].album.images[0].url + "'>";
-            searchResultsDiv.innerHTML += "<div class='col-lg-3'><div class='panel panel-default'><div class='panel-heading'><div class='row'><div class='col-lg-9'>" + tracksList[i].name + " <a href='" + tracksList[i].external_urls.spotify +"' target='_blank'><span class='glyphicon glyphicon-new-window' aria-hidden='true'></span></a>" + preview_link + "<br>Artist: " + tracksList[i].artists[0].name + "</div><div class='col-lg-3'>" + artwork + "</div></div><div class='panel-body'>" + attributes + "</div></div></div>";
+            // check if album image exists before the tag is created for it
+            if (tracksList[i].album.images[0].url) {
+              var artwork = "<img src='" + tracksList[i].album.images[0].url + "'>";
+            }
+            else {
+              var artwork = "";
+            }
+            // check if the preview exists before building a link for it
+            if (tracksList[i].preview_url) {
+              var preview_link = " <a href='" + tracksList[i].preview_url + "' target='_blank'><span class='glyphicon glyphicon-music' aria-hidden='true'></span></a>";
+              searchResultsDiv.innerHTML += "<div class='col-lg-3'><div class='panel panel-default'><div class='panel-heading'><div class='row'><div class='col-lg-9'>" + tracksList[i].name + " <a href='" + tracksList[i].external_urls.spotify +"' target='_blank'><span class='glyphicon glyphicon-new-window' aria-hidden='true'></span></a>" + preview_link + "<br>Artist: " + tracksList[i].artists[0].name + "</div><div class='col-lg-3'>" + artwork + "</div></div></div><div class='panel-body'>" + attributes + "</div></div>";
+            }
+            else {
+              searchResultsDiv.innerHTML += "<div class='col-lg-3'><div class='panel panel-default'><div class='panel-heading'><div class='row'><div class='col-lg-9'>" + tracksList[i].name + "<a href='" + tracksList[i].external_urls.spotify + "' target='_blank'><span class='glyphicon glyphicon-new-window' aria-hidden='true'></span></a><br>Artist: " + tracksList[i].artists[0].name + "</div><div class='col-lg-3'>" + artwork + "</div></row></div></div><div class='panel-body'>" + attributes + "</div></div>";
+
+            }
           }
         }
       });
-
     }
   });
 }
